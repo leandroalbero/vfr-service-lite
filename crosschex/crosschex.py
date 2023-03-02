e8e752acb5571152d818d324505129ccad4e9773
Leandro Albero <leandro.albero@jobandtalent.com>
2023-03-02 16:52:58 +0100

    Move config class

--- crosschex/crosschex.py
+++ crosschex/crosschex.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
 import json
+import os
 import threading
 from typing import Any, List
 
 import requests
-from .config import get_settings
 from requests.adapters import HTTPAdapter
 
 API_EMPLOYEE_SEARCH = "employee/grid"
@@ -13,6 +13,32 @@ API_EMPLOYEE_FACE_UPDATE = "employee/attendance/face/update"
 API_USER_LOGIN = "user/login"
 API_LIST_COMPANY = "company/list"
 API_SELECT_COMPANY = "company/select"
+
+
+class Config(object):
+    """Base configuration."""
+
+    SETTINGS_DIR = os.path.abspath('..')  # This directory
+    APP_DIR = os.path.abspath(os.path.join(SETTINGS_DIR, os.pardir))  # Parent dir
+    PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))  # Parent dir
+    SERVICE_NAME = os.environ.get("APP_NAME", "vfr")
+
+    API_HOST = "eu.crosschexcloud.com"
+    API_URL = f"https://{API_HOST}/api"
+    API_TIMEOUT = 120
+    # get token from collab secrets
+    ACCOUNT_EMAIL = os.environ.get("ACCOUNT_EMAIL", "")
+    ACCOUNT_PASSWORD = os.environ.get("ACCOUNT_PASSWORD", "")
+    ACCOUNT_NAME = 'test'
+
+    PATH_DOWNLOAD_JSON = f"data/downloads/{ACCOUNT_NAME}.json"
+    PATH_EXPORT_CSV = f"data/exports/{ACCOUNT_NAME}.csv"
+
+    DELAY_SECONDS = 5
+
+
+def get_settings():  # type: ignore
+    return Config
 
 
 class CrossChexCloudAdapter(HTTPAdapter):
