1bbf11d887c0d1a8393e93616e051ba64d3342c8
Leandro Albero <leandro.albero@jobandtalent.com>
2023-03-06 17:12:40 +0100

    Remove class config

--- crosschex/crosschex.py
+++ crosschex/crosschex.py
@@ -13,32 +13,23 @@ API_EMPLOYEE_FACE_UPDATE = "employee/attendance/face/update"
 API_USER_LOGIN = "user/login"
 API_LIST_COMPANY = "company/list"
 API_SELECT_COMPANY = "company/select"
+SETTINGS_DIR = os.path.abspath('..')  # This directory
+APP_DIR = os.path.abspath(os.path.join(SETTINGS_DIR, os.pardir))  # Parent dir
+PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))  # Parent dir
+SERVICE_NAME = os.environ.get("APP_NAME", "vfr")
 
+API_HOST = "eu.crosschexcloud.com"
+API_URL = f"https://{API_HOST}/api"
+API_TIMEOUT = 120
+# get token from collab secrets
+ACCOUNT_EMAIL = os.environ.get("ACCOUNT_EMAIL", "")
+ACCOUNT_PASSWORD = os.environ.get("ACCOUNT_PASSWORD", "")
+ACCOUNT_NAME = 'test'
 
-class Config(object):
-    """Base configuration."""
+PATH_DOWNLOAD_JSON = f"data/downloads/{ACCOUNT_NAME}.json"
+PATH_EXPORT_CSV = f"data/exports/{ACCOUNT_NAME}.csv"
 
-    SETTINGS_DIR = os.path.abspath('..')  # This directory
-    APP_DIR = os.path.abspath(os.path.join(SETTINGS_DIR, os.pardir))  # Parent dir
-    PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))  # Parent dir
-    SERVICE_NAME = os.environ.get("APP_NAME", "vfr")
-
-    API_HOST = "eu.crosschexcloud.com"
-    API_URL = f"https://{API_HOST}/api"
-    API_TIMEOUT = 120
-    # get token from collab secrets
-    ACCOUNT_EMAIL = os.environ.get("ACCOUNT_EMAIL", "")
-    ACCOUNT_PASSWORD = os.environ.get("ACCOUNT_PASSWORD", "")
-    ACCOUNT_NAME = 'test'
-
-    PATH_DOWNLOAD_JSON = f"data/downloads/{ACCOUNT_NAME}.json"
-    PATH_EXPORT_CSV = f"data/exports/{ACCOUNT_NAME}.csv"
-
-    DELAY_SECONDS = 5
-
-
-def get_settings():  # type: ignore
-    return Config
+DELAY_SECONDS = 5
 
 
 class CrossChexCloudAdapter(HTTPAdapter):
@@ -47,7 +38,6 @@ class CrossChexCloudAdapter(HTTPAdapter):
 
     # Singleton pattern
     def __new__(cls, *args: Any, **kwargs: Any) -> CrossChexCloudAdapter:
-        cls.config = get_settings()
         if not cls.__instance:
             with cls.__lock:
                 # Double check because if multithreading another thread
@@ -76,40 +66,40 @@ class CrossChexCloudAdapter(HTTPAdapter):
             "x-api-version": "2.0",
         }
         payload = {
-            "email": self.config.ACCOUNT_EMAIL,
-            "password": self.config.ACCOUNT_PASSWORD,
+            "email": ACCOUNT_EMAIL,
+            "password": ACCOUNT_PASSWORD,
         }
-        url = f"{self.config.API_URL}/{API_USER_LOGIN}"
+        url = f"{API_URL}/{API_USER_LOGIN}"
         response = requests.post(
             url=url,
             headers=auth_header,
             data=json.dumps(payload),
-            timeout=self.config.API_TIMEOUT,
+            timeout=API_TIMEOUT,
         )
         self.token = response.json()["data"]["token"]  # JWT token
         self.company_id = int(response.json()["data"]["company_id"])
-        print(f"Anviz API Cloud: user token created: {self.config.ACCOUNT_NAME}")
+        print(f"Anviz API Cloud: user token created: {ACCOUNT_NAME}")
 
     def _list_company(self) -> None:
         payload = {}
-        url = f"{self.config.API_URL}/{API_LIST_COMPANY}"
+        url = f"{API_URL}/{API_LIST_COMPANY}"
         response = requests.post(
             url=url,
             headers=self.headers,
             data=json.dumps(payload),
-            timeout=self.config.API_TIMEOUT,
+            timeout=API_TIMEOUT,
         )
         self.company_cloud_id = response.json()["data"][0]["id"]  # company cloud id
         print(f"Anviz API Cloud: company list: {self.company_cloud_id}")
 
     def _select_company(self) -> None:
         payload = {"id": self.company_cloud_id}
-        url = f"{self.config.API_URL}/{API_SELECT_COMPANY}"
+        url = f"{API_URL}/{API_SELECT_COMPANY}"
         response = requests.post(
             url=url,
             headers=self.headers,
             data=json.dumps(payload),
-            timeout=self.config.API_TIMEOUT,
+            timeout=API_TIMEOUT,
         )
         # update token when select company
         self.token = response.json()["data"]["token"]  # JWT token
@@ -119,12 +109,12 @@ class CrossChexCloudAdapter(HTTPAdapter):
         raise NotImplementedError
 
     def post(self, path: str, body: dict = {}) -> dict:
-        url = f"{self.config.API_URL}/{path}"
+        url = f"{API_URL}/{path}"
         response = requests.post(
             url=url,
             headers=self.headers,
             data=json.dumps(body),
-            timeout=self.config.API_TIMEOUT,
+            timeout=API_TIMEOUT,
         )
         response.raise_for_status()
         return response.json()
@@ -135,13 +125,13 @@ class CrossChexCloudAdapter(HTTPAdapter):
             "x-api-version": "2.0",
             "x-token": self.token,
         }
-        url = f"{self.config.API_URL}/{path}"
+        url = f"{API_URL}/{path}"
         response = requests.post(
             url=url,
             headers=headers,
             data=fields,
             files=files,
-            timeout=self.config.API_TIMEOUT,
+            timeout=API_TIMEOUT,
         )
         response.raise_for_status()
         return response.json()
